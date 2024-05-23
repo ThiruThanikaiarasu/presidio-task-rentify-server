ee22a07d353ee5f91f8255a5a735eac568a8b515
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-05-23 14:47:15 +0530

    ADD. Established the mongoDB Atlas connection.

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 
 const { PORT } = require('./configuration/config')
+const connect = require('./database/connection')
 
 app.get('/', (request, response) => {
     response.status(200).send({ message: "it's working ✌️"})
 })
 
-app.listen(PORT, console.log(`Server is listening at http://localhost:${PORT}`))
+connect() 
+    .then( () => {
+        try{
+            app.listen(PORT, console.log(`Server is running at http://localhost:${PORT}`))
+        } 
+        catch(error) {
+            console.log(`Can't connect to database : ${error}`)
+        }
+    })
+    .catch(error => {
+        console.log(`Error while connecting to database : ${error}`)
+    })
